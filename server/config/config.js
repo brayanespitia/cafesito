08c82b29e09db2a6af3fc6f4a0fc83c33e608fc0
brayanespitia <brayanespitia2013@gmail.com>
2020-08-04 23:29:42 -0500

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -14,6 +14,6 @@ let urlDB;
 if (process.env.NODE_ENV === "dev") {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = "mongodb+srv://ufps:web123@cluster0.3mprl.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
